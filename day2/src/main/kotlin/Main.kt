6028adb5ff278d80ed172a701884de755f7cf9f2
oscar <osopenmo7@outlook.com>
2024-12-09 18:15:03 +0100

    added a couple comments

--- day2/src/main/kotlin/Main.kt
+++ day2/src/main/kotlin/Main.kt
@@ -5,14 +5,16 @@ import kotlin.io.path.readLines
 
 fun main() {
     val reportList: Sequence<List<Int>> = read("day2/Input.txt")
-    reportList.getLevelDiffs().count(::isSafeReport).also(::println)
-    reportList.count { report -> isSafeReport(report) || isSafeReportDampened(report) }.also(::println)
+    reportList.map { levels -> levels.getLevelDiffs() }.count(::isSafeReport).also(::println) //this is correct: 279
+    reportList.map { levels -> levels.getLevelDiffs() }
+        .count { report -> isSafeReport(report) || isSafeReportDampened(report) }
+        .also(::println) // should be smaller than 350!!
 }
 
 fun read(file: String): Sequence<List<Int>> =
     Path(file).readLines().asSequence().map { str -> str.split(" ").map(String::toInt) }
 
-fun Sequence<List<Int>>.getLevelDiffs(): Sequence<List<Int>> = map { report -> report.zipWithNext { a, b -> b - a } }
+fun List<Int>.getLevelDiffs(): List<Int> = zipWithNext { a, b -> b - a }
 
 fun isSafeReport(levelDiffs: List<Int>): Boolean = levelDiffs.all { it > 0 && it in (1..3) } ||
         levelDiffs.all { it < 0 && -it in (1..3) }
