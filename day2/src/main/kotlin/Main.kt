990ef2e9f6f27288611cdc9399c73def43520b05
oscar <osopenmo7@outlook.com>
2024-12-09 17:34:18 +0100

    part 2 failed attempt

--- day2/src/main/kotlin/Main.kt
+++ day2/src/main/kotlin/Main.kt
@@ -4,14 +4,19 @@ import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
 fun main() {
-    val reportListLevelDiffs: Sequence<List<Int>> =
-        read("day2/Input.txt").asSequence()
-            .map { str -> str.split(" ").map(String::toInt) }
-            .map { ints -> ints.zipWithNext { a, b -> b - a } }
-    reportListLevelDiffs.map(::isSafeReport).count { it == true }.also(::println)
+    val reportList: Sequence<List<Int>> = read("day2/Input.txt")
+    reportList.getLevelDiffs().count(::isSafeReport).also(::println)
+    reportList.count { report -> isSafeReport(report) || isSafeReportDampened(report) }.also(::println)
 }
 
-fun read(file: String) = Path(file).readLines()
+fun read(file: String): Sequence<List<Int>> =
+    Path(file).readLines().asSequence().map { str -> str.split(" ").map(String::toInt) }
+
+fun Sequence<List<Int>>.getLevelDiffs() = map { report -> report.zipWithNext { a, b -> b - a } }
 
 fun isSafeReport(levelDiffs: List<Int>): Boolean = levelDiffs.all { it > 0 && it in (1..3) } ||
         levelDiffs.all { it < 0 && -it in (1..3) }
+
+fun isSafeReportDampened(levelDiffs: List<Int>): Boolean =
+    levelDiffs.count { it > 0 && it in (1..3) } == levelDiffs.size - 1 ||
+            levelDiffs.count { it < 0 && -it in (1..3) } == levelDiffs.size - 1
