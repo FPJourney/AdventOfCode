942a95d25e937696bf04364d82a526963334b091
oscar <osopenmo7@outlook.com>
2024-12-11 19:49:35 +0100

    convert isSafeReport to extension function

--- day2/src/main/kotlin/Main.kt
+++ day2/src/main/kotlin/Main.kt
@@ -5,9 +5,9 @@ import kotlin.io.path.readLines
 
 fun main() {
     val reportList: Sequence<List<Int>> = read("day2/Input.txt")
-    reportList.map { levels -> levels.getLevelDiffs() }.count(::isSafeReport).also(::println) //this is correct: 279
-    reportList.map { levels -> levels.getLevelDiffs() }
-        .count { report -> isSafeReport(report) || isSafeReportDampened(report) }
+    reportList.count { it.isSafeReport() }.also(::println) //this is correct: 279
+    reportList
+        .count(::isSafeReportDampened)
         .also(::println) // should be smaller than 350!!
 }
 
@@ -16,9 +16,15 @@ fun read(file: String): Sequence<List<Int>> =
 
 fun List<Int>.getLevelDiffs(): List<Int> = zipWithNext { a, b -> b - a }
 
-fun isSafeReport(levelDiffs: List<Int>): Boolean = levelDiffs.all { it > 0 && it in (1..3) } ||
-        levelDiffs.all { it < 0 && -it in (1..3) }
+fun List<Int>.isSafeReport(): Boolean {
+    val levelDiffs = getLevelDiffs()
+    return levelDiffs.all { it > 0 && it in (1..3) } ||
+            levelDiffs.all { it < 0 && -it in (1..3) }
+}
 
-fun isSafeReportDampened(levelDiffs: List<Int>): Boolean =
-    levelDiffs.count { it > 0 && it in (1..3) } == levelDiffs.size - 1 ||
-            levelDiffs.count { it < 0 && -it in (1..3) } == levelDiffs.size - 1
+//Fixme: current value is 357 but should be smaller than 350
+fun isSafeReportDampened(report: List<Int>): Boolean {
+    val levelDiffs = report.getLevelDiffs()
+    return levelDiffs.isSafeReport() ||
+            report.any { level -> (report - level).isSafeReport() }
+}
