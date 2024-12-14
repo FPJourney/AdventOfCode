a4277eade185ffdc2cae43ab872cfb2d38aa5cb1
oscar <osopenmo7@outlook.com>
2024-12-15 00:13:52 +0100

    added empty test

--- day2/src/test/kotlin/TestMain.kt
+++ day2/src/test/kotlin/TestMain.kt
@@ -1,7 +1,10 @@
+import org.example.isSafeDampenedReport
 import kotlin.test.Test
 
 class TestMain {
     @Test
-    fun `is Dampened report test`() {
+    fun `is Dampened safe report test`() {
+        val report = "2 4 6 2 7".split(" ").map { it.toInt() }
+        assert(report.isSafeDampenedReport())
     }
 }
