39ab6e53af3d7e257d3ffc28b52f35fcd56256dd
oscar <osopenmo7@outlook.com>
2024-12-15 02:11:57 +0100

    add return type in calculateMul

--- day3/src/main/kotlin/Main.kt
+++ day3/src/main/kotlin/Main.kt
@@ -6,7 +6,7 @@ import kotlin.io.path.readLines
 fun main() {
     val multsLists = read("day3/Input.txt")
     val multResultsLists: Sequence<List<Long>> = multsLists.map { strings -> strings.map(::calculateMul) }
-    val summedResultsList = multResultsLists.map { it.reduce { a, b -> a + b } }
+    val summedResultsList = multResultsLists.map { it.sum() }
     // summedResultsList.forEach(::println)
     println(summedResultsList.sum())
 }
@@ -15,4 +15,5 @@ fun read(file: String): Sequence<List<String>> =
     Path(file).readLines().asSequence()
         .map { str -> Regex("mul\\(\\d{1,3},\\d{1,3}\\)").findAll(str).map { it.value }.toList() }
 
-fun calculateMul(mul: String) = Regex("\\d{1,3}").findAll(mul).map { it.value.toLong() }.reduce { a, b -> a * b }
+fun calculateMul(mul: String): Long =
+    Regex("\\d{1,3}").findAll(mul).map { it.value.toLong() }.reduce { a, b -> a * b }
