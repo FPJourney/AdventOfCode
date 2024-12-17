dc2c1808f3fae269510e8bfe24fc9977d804f305
oscar <osopenmo7@outlook.com>
2024-12-17 21:56:57 +0100

    small change to day3 part1 + attempt 2 at part 2

--- day3/src/main/kotlin/Main.kt
+++ day3/src/main/kotlin/Main.kt
@@ -7,23 +7,35 @@ fun main() {
     val part1Regex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
     val multsLists = read("day3/Input.txt")
     multsLists.parseInput(part1Regex)
-        .flatMap { strings -> strings.map(::calculateMul) }
+        .map(::calculateMul)
         .sum()
         .also(::println)
-    val part2Regex = Regex("do\\(\\)|don't\\(\\)|mul\\(\\d{1,3},\\d{1,3}\\)")
+    val part2Regex = Regex("""do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)""")
     multsLists.parseInput(part2Regex)
-        .flatMap { strings -> strings.filterNot { it == "do()" || it == "don't()" }.map(::calculateMul) }
+        .fold(Pair(true, 0L), ::calculateMulConditionally)
         //.map { it.sum() }
-        .sum()
+//        .sum()
         .also(::println)
 }
 
 fun read(file: String): Sequence<String> =
     Path(file).readLines().asSequence()
 
-
-fun Sequence<String>.parseInput(regex: Regex): Sequence<List<String>> =
-    map { str -> regex.findAll(str).map { it.value }.toList() }
+fun Sequence<String>.parseInput(regex: Regex): Sequence<String> =
+    flatMap { str -> regex.findAll(str).map { it.value } }
 
 fun calculateMul(mul: String): Long =
     Regex("\\d{1,3}").findAll(mul).map { it.value.toLong() }.reduce { a, b -> a * b }
+
+fun calculateMulConditionally(acc: Pair<Boolean, Long>, str: String): Pair<Boolean, Long> {
+    val (flag, sum) = acc
+    return when (str) {
+        "do()" -> Pair(true, sum)
+        "don't" -> Pair(false, sum)
+        else -> when (Pair(flag, sum)) {
+            Pair(true, sum) -> Pair(true, sum + calculateMul(str))
+            Pair(false, sum) -> Pair(false, sum)
+            else -> acc
+        }
+    }
+}
