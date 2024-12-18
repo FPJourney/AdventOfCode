2a68845177176ab2711b25f03d20e537c6de62b6
oscar <osopenmo7@outlook.com>
2024-12-18 01:10:49 +0100

    day 4 part 1 second draft

--- day4/src/main/kotlin/Main.kt
+++ day4/src/main/kotlin/Main.kt
@@ -7,7 +7,7 @@ fun main() {
     val pattern = "XMAS".toRegex()
     val lines = read("day4/Input.txt")
     lines.flatMap { line -> pattern.findAll(line).map { it.value } }.count().also(::println)
-    lines.flatMap { line -> pattern.findAll(line).map { it.value } }.count().also(::println)
+    lines.flatMap { line -> pattern.findAll(line.reversed()).map { it.value } }.count().also(::println)
 }
 
 fun read(file: String): Sequence<String> =
