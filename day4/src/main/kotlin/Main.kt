36deb2b61ecc80103d790811b7cc8054d4064fbe
oscar <osopenmo7@outlook.com>
2024-12-18 02:10:01 +0100

    measured input size and looked for pattern value

--- day4/src/main/kotlin/Main.kt
+++ day4/src/main/kotlin/Main.kt
@@ -5,7 +5,12 @@ import kotlin.io.path.readLines
 
 fun main() {
     val pattern = "XMAS".toRegex()
+    val patternValue = "XMAS".sumOf { it.code } //313
+//    println("pattern value is $patternValue")
     val lines = read("day4/Input.txt")
+    //Input is a 140 * 140 matrix
+//    lines.first().count().also(::println)
+//    lines.count().also(::println)
     lines.flatMap { line -> pattern.findAll(line).map { it.value } }.count().also(::println)
     lines.flatMap { line -> pattern.findAll(line.reversed()).map { it.value } }.count().also(::println)
 }
